c15dace5e3095df7da99d585779aa9d1f00bad72
Francisco <franciscorios0610@gmail.com>
2023-02-24 02:19:56 -0500

    chore: comment metatags

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -2,8 +2,6 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
-    console.log("MyDocument", ctx);
-
     const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
@@ -12,8 +10,8 @@ export default class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          {/* <link rel="icon" href="/favicon.ico" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" /> */}
         </Head>
         <body>
           <Main />
--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -4,11 +4,11 @@ export default function Web() {
   return (
     <div>
       <UI.Heading as="h1">Web</UI.Heading>
-      <UI.Text>Lorem ipsum</UI.Text>
-      <UI.Text>
+      <UI.Text marginBottom={2}>Lorem ipsum</UI.Text>
+      <UI.Flex as="p" gap={1} alignItems="center">
         <CIcon icon={icon.riGithubFill} size="xl" />
         Lorem ipsu
-      </UI.Text>
+      </UI.Flex>
     </div>
   );
 }
