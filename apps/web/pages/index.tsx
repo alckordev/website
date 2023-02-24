2a81894a8059f57cb0d4810225b2e28328c7c1dd
Francisco <franciscorios0610@gmail.com>
2023-02-24 01:43:15 -0500

    chore: test icons

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,14 @@
-import { UI } from "ui";
+import { UI, CIcon, icon } from "ui";
 
 export default function Web() {
   return (
     <div>
       <UI.Heading as="h1">Web</UI.Heading>
       <UI.Text>Lorem ipsum</UI.Text>
+      <UI.Text>
+        <CIcon icon={icon.riGithubFill} size="xl" />
+        Lorem ipsu
+      </UI.Text>
     </div>
   );
 }
