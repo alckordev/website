4cffae14e7b673726903235c134f96b838790580
Francisco <franciscorios0610@gmail.com>
2023-02-24 02:19:27 -0500

    chore: add styles for icons

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -6,3 +6,4 @@ export {
 } from "./src/chakra-theme";
 export * as UI from "./src/chakra-ui";
 export { CIcon, icon } from "./src/coreui-icon";
+export * as fonts from "./src/fonts";
--- packages/ui/src/coreui-icon.tsx
+++ packages/ui/src/coreui-icon.tsx
@@ -1,4 +1,6 @@
 import CIcon from "@coreui/icons-react";
 import * as icon from "./icons";
 
+import "./styles/coreui-icon.css";
+
 export { CIcon, icon };
