bdd4e15d61efd65763d686933bf8a7dead71b3bc
Francisco <franciscorios0610@gmail.com>
2023-02-24 01:13:06 -0500

    feat: add chakra theme provider

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,2 +1,7 @@
-import * as React from "react";
+// import * as React from "react";
+export {
+  ThemeProvider,
+  theme,
+  createLocalStorageManager,
+} from "./src/chakra-theme";
 export * as UI from "./src/chakra-ui";
