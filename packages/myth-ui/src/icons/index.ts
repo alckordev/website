1064e3b724702f20dd5012cb46b96309dfd2e5e3
Francisco <franciscorios0610@gmail.com>
2023-02-24 19:30:48 -0500

    chore: add new icons

--- packages/myth-ui/src/icons/index.ts
+++ packages/myth-ui/src/icons/index.ts
@@ -1,12 +1,14 @@
 export { riArticleLine } from "./ri-article-line";
 export { riArrowRightLine } from "./ri-arrow-right-line";
 export { riCalendarLine } from "./ri-calendar-line";
+export { riCloseLine } from "./ri-close-line";
 export { riDiscussLine } from "./ri-discuss-line";
 export { riFacebookLine } from "./ri-facebook-line";
 export { riGithubLine } from "./ri-github-line";
 export { riHomeLine } from "./ri-home-line";
 export { riInstagramLine } from "./ri-instagram-line";
 export { riLinkedinLine } from "./ri-linkedin-line";
+export { riMenuLine } from "./ri-menu-line";
 export { riTimeLine } from "./ri-time-line";
 export { riYoutubeLine } from "./ri-youtube-line";
 
