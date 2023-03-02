ba1246f608e8ecdf52d9eb4976ea0a2f7a7e8ed1
Francisco <franciscorios0610@gmail.com>
2023-03-01 22:23:22 -0500

    chore: footer

--- apps/web/data/networks.ts
+++ apps/web/data/networks.ts
@@ -1,28 +1,34 @@
 import { icon as svg } from "@myth/ui";
 
 interface Type {
+  name: string;
   icon: string[];
   url: string;
 }
 
 const networks: Type[] = [
   {
+    name: "Youtube",
     icon: svg.riYoutubeLine,
     url: "https://www.youtube.com/@alckordev?sub_confirmation=1",
   },
   {
+    name: "Facebook",
     icon: svg.riFacebookLine,
     url: "https://www.facebook.com/@alckor.dev",
   },
   {
+    name: "Instagram",
     icon: svg.riInstagramLine,
     url: "https://www.instagram.com/alckor.dev",
   },
   {
+    name: "Github",
     icon: svg.riGithubLine,
     url: "https://github.com/alckordev",
   },
   {
+    name: "Linkedin",
     icon: svg.riLinkedinLine,
     url: "https://www.linkedin.com/in/alckordev",
   },
--- packages/myth-ui/src/components/Footer.tsx
+++ packages/myth-ui/src/components/Footer.tsx
@@ -3,12 +3,15 @@ import {
   Box,
   Flex,
   Link,
+  ButtonGroup,
+  IconButton,
   useColorModeValue,
 } from "@chakra-ui/react";
 import CIcon from "@coreui/icons-react";
 
 interface Props {
   networks?: {
+    name: string;
     icon: string[];
     url: string;
   }[];
@@ -19,26 +22,20 @@ export const Footer = ({ networks = [], ...rest }: Props) => {
     <Box as="footer" bg={useColorModeValue("gray.100", "gray.900")}>
       <Container maxW="container.xl" py={16}>
         {networks.length > 0 && (
-          <Flex w="100%" justify="center" align="center" pb={8} gap={3}>
+          <ButtonGroup display="flex" justifyContent="center" pb={10}>
             {networks.map((n, idx) => (
-              <Link
+              <IconButton
                 key={idx}
+                as={Link}
+                colorScheme="purple"
+                aria-label={n.name}
+                icon={<CIcon icon={n.icon} />}
+                variant="outline"
                 href={n.url}
-                target="_blank"
-                display="inline-flex"
-                // bg="gray.900"
-                p={2}
-                // color="gray.500"
-                rounded="md"
-                // _hover={{
-                //   color: "gray.400",
-                //   textDecoration: "none",
-                // }}
-              >
-                <CIcon icon={n.icon} />
-              </Link>
+                isExternal
+              />
             ))}
-          </Flex>
+          </ButtonGroup>
         )}
 
         <Flex
