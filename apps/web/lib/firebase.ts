0205a465d397d662de88f3e89e38604539be2d83
Francisco <franciscorios0610@gmail.com>
2023-03-10 00:56:54 -0500

    feat: enable auth

--- apps/web/lib/firebase.ts
+++ apps/web/lib/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
@@ -14,5 +15,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
+const auth = getAuth(app);
 
-export { database };
+export { database, auth };
