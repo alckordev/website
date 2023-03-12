19f4375879898d40a87ffee5bd88d00d0cf6a687
Francisco <franciscorios0610@gmail.com>
2023-03-12 10:49:20 -0500

    chore: logout message

--- apps/web/components/Auth/SignOutButton.tsx
+++ apps/web/components/Auth/SignOutButton.tsx
@@ -1,18 +1,32 @@
-import { UI } from "@myth/ui";
+import { useState } from "react";
+import { UI, useToast } from "@myth/ui";
 import { auth } from "../../lib/firebase";
 
 export const SignOutButton = ({ ...rest }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const toast = useToast({ position: "top" });
+
   const handleSignOut = async () => {
+    setIsLoading(true);
     try {
       await auth.signOut();
-      console.log("signOut");
+
+      toast({
+        description: "¡Hasta la próxima! Esperamos verte pronto de nuevo.",
+      });
     } catch (error) {
-      console.log("signOut", error);
+      toast({
+        description: "¡Ups! Algo salió mal. 😭",
+        status: "error",
+        isClosable: true,
+      });
     }
+    setIsLoading(false);
   };
 
   return (
-    <UI.Button onClick={handleSignOut} {...rest}>
+    <UI.Button onClick={handleSignOut} isLoading={isLoading} {...rest}>
       Cerrar sesión
     </UI.Button>
   );
