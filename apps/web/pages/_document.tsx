18fd64350843911ef1bc56b6d6e9ef38bff77594
Francisco <franciscorios0610@gmail.com>
2023-03-24 08:12:03 -0500

    Chore: Add Favicons

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -9,7 +9,32 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
-        <Head />
+        <Head>
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/assets/favicon-16x16.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/assets/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="64x64"
+            href="/assets/favicon-64x64.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="128x128"
+            href="/assets/favicon-128x128.png"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
