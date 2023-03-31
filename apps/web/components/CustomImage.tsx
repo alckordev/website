9e5cfe945339c855d637cbce4f9934cd57fc1e47
Francisco <franciscorios0610@gmail.com>
2023-03-30 23:59:33 -0500

    Fix: :bug: Fixing color caption in CustomImage

--- apps/web/components/CustomImage.tsx
+++ apps/web/components/CustomImage.tsx
@@ -1,18 +1,14 @@
 // import Image, { ImageProps } from "next/image";
-import { UI } from "@myth/ui";
+import { UI, useColorModeValue } from "@myth/ui";
 
 export const CustomImage = (props: any) => {
+  const color = useColorModeValue("blackAlpha.700", "whiteAlpha.700");
+
   return (
     <UI.Box my={8}>
       <UI.Image src={props.src} alt={props.alt} rounded="md" boxShadow="md" />
       {props.caption && (
-        <UI.Text
-          as="em"
-          display="block"
-          fontSize="sm"
-          color="whiteAlpha.700"
-          mt={4}
-        >
+        <UI.Text as="em" display="block" fontSize="sm" color={color} mt={4}>
           {props.caption}
         </UI.Text>
       )}
