4be697c0006ca5ef56df09310d914379f6bb85fa
Francisco <franciscorios0610@gmail.com>
2023-04-04 20:36:36 -0500

    Fix: Fixing size logo

--- packages/myth-ui/src/components/NavBarFullScreen/Logo.tsx
+++ packages/myth-ui/src/components/NavBarFullScreen/Logo.tsx
@@ -4,12 +4,13 @@ export const Logo = () => {
   const { colorMode } = useColorMode();
 
   return (
-    <Link href="/" _hover={{ textDecor: "none" }}>
+    <Link href="/" _hover={{ textDecor: "none" }} flex="0 0 113px" maxW="113px">
       <Flex>
         <Image
           src={
             colorMode === "dark" ? "/assets/logo-dark.png" : "/assets/logo.png"
           }
+          width="113px"
           height="30px"
           alt="Logo"
         />
