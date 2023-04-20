6431994d65eb12e8118aee482b31268915fa58f4
Francisco <franciscorios0610@gmail.com>
2023-04-20 09:30:41 -0500

    Chore: Add vercel analytic

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,16 +1,18 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider, theme } from "@myth/ui";
-import { AuthProvider } from "../store/AuthProvider";
+import { Analytics } from "@vercel/analytics/react";
+import { AuthContextProvider } from "../store/AuthContextProvider";
 
 import "@myth/ui/src/fonts";
 import "../styles/prism-night-owl.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <AuthProvider>
+    <AuthContextProvider>
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
-    </AuthProvider>
+    </AuthContextProvider>
   );
 }
