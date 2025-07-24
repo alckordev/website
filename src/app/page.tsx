951a6cfa3201cffb93277f0d322e7070a7bb373c
Alckor <alckordev@gmail.com>
2025-07-24 16:48:39 -0500

    style(ui): :lipstick: change tailwind to mantine ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@ export default function Home() {
   return (
     <div>
       <h1>Hello world</h1>
-      <button className="btn">
-        <span className="loading loading-spinner"></span>
-        loading
-      </button>
     </div>
   );
 }
